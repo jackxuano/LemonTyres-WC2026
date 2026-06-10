c85e45552128924e07e59ed7e36293d1bca2bd35
jackxuano <jackxuan.ng@gmail.com>
2026-06-10 16:38:40 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -10,7 +10,7 @@ const PLAYERS = [
     team: 'Portugal', teamCode: 'Portugal', flag: '🇵🇹', rank: 5, cls: 'A',
     hero: 'Lautaro Martinez', heroNation: 'Argentina', heroTop10: true, isCR7: false,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Lautaro_Mart%C3%ADnez_2022_%28cropped%29.jpg/240px-Lautaro_Mart%C3%ADnez_2022_%28cropped%29.jpg',
+    heroImg: 'img/lautaro.jpg',
     heroInitials: 'LM',
   },
   {
@@ -18,7 +18,7 @@ const PLAYERS = [
     team: 'Iran', teamCode: 'Iran', flag: '🇮🇷', rank: 21, cls: 'B',
     hero: 'Vinícius Júnior', heroNation: 'Brazil', heroTop10: true, isCR7: false,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Vinicius_Junior_2022_WC.jpg/240px-Vinicius_Junior_2022_WC.jpg',
+    heroImg: 'img/vinicius.jpg',
     heroInitials: 'VJ',
   },
   {
@@ -26,7 +26,7 @@ const PLAYERS = [
     team: 'Japan', teamCode: 'Japan', flag: '🇯🇵', rank: 18, cls: 'B',
     hero: 'Marcus Rashford', heroNation: 'England', heroTop10: true, isCR7: false,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Marcus_Rashford_2018.jpg/240px-Marcus_Rashford_2018.jpg',
+    heroImg: 'img/rashford.jpg',
     heroInitials: 'MR',
   },
   {
@@ -34,7 +34,7 @@ const PLAYERS = [
     team: 'Sweden', teamCode: 'Sweden', flag: '🇸🇪', rank: 38, cls: 'C',
     hero: 'Bruno Fernandes', heroNation: 'Portugal', heroTop10: true, isCR7: false,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Bruno_Fernandes_-_2020_%28cropped%29.jpg/240px-Bruno_Fernandes_-_2020_%28cropped%29.jpg',
+    heroImg: 'img/bruno.jpg',
     heroInitials: 'BF',
   },
   {
@@ -42,7 +42,7 @@ const PLAYERS = [
     team: 'Bosnia', teamCode: 'Bosnia & Herzegovina', flag: '🇧🇦', rank: 65, cls: 'D',
     hero: 'Darwin Núñez', heroNation: 'Uruguay', heroTop10: false, isCR7: false,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Darwin_N%C3%BA%C3%B1ez_2022.jpg/240px-Darwin_N%C3%BA%C3%B1ez_2022.jpg',
+    heroImg: 'img/darwin.jpg',
     heroInitials: 'DN',
   },
   {
@@ -50,7 +50,7 @@ const PLAYERS = [
     team: 'Argentina', teamCode: 'Argentina', flag: '🇦🇷', rank: 3, cls: 'A',
     hero: 'Erling Haaland', heroNation: 'Norway', heroTop10: false, isCR7: false,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Erling_Haaland_2023_%28cropped%29.jpg/240px-Erling_Haaland_2023_%28cropped%29.jpg',
+    heroImg: 'img/haaland.jpg',
     heroInitials: 'EH',
   },
   {
@@ -58,7 +58,7 @@ const PLAYERS = [
     team: 'Japan', teamCode: 'Japan', flag: '🇯🇵', rank: 18, cls: 'B',
     hero: 'Alexander Sørloth', heroNation: 'Norway', heroTop10: false, isCR7: false,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Alexander_S%C3%B8rloth_2023_%28cropped%29.jpg/240px-Alexander_S%C3%B8rloth_2023_%28cropped%29.jpg',
+    heroImg: 'img/sorloth.jpg',
     heroInitials: 'AS',
   },
   {
@@ -66,7 +66,7 @@ const PLAYERS = [
     team: 'Netherlands', teamCode: 'Netherlands', flag: '🇳🇱', rank: 8, cls: 'A',
     hero: 'Kylian Mbappé', heroNation: 'France', heroTop10: true, isCR7: false,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/2019-07-17_SG_Dynamo_Dresden_vs._Paris_Saint-Germain_FC_by_Sandro_Halank%E2%80%93149_%28cropped%29.jpg/240px-2019-07-17_SG_Dynamo_Dresden_vs._Paris_Saint-Germain_FC_by_Sandro_Halank%E2%80%93149_%28cropped%29.jpg',
+    heroImg: 'img/mbappe.jpg',
     heroInitials: 'KM',
   },
   {
@@ -75,7 +75,7 @@ const PLAYERS = [
     team: 'Croatia', teamCode: 'Croatia', flag: '🇭🇷', rank: 11, cls: 'B',
     hero: 'Lamine Yamal', heroNation: 'Spain', heroTop10: true, isCR7: false,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Lamine_Yamal_2024_%28cropped%29.jpg/240px-Lamine_Yamal_2024_%28cropped%29.jpg',
+    heroImg: 'img/yamal.jpg',
     heroInitials: 'LY',
   },
   {
@@ -83,7 +83,7 @@ const PLAYERS = [
     team: 'Spain', teamCode: 'Spain', flag: '🇪🇸', rank: 2, cls: 'A',
     hero: 'Cristiano Ronaldo', heroNation: 'Portugal', heroTop10: true, isCR7: true,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Cristiano_Ronaldo_WC2022_-_01.jpg/240px-Cristiano_Ronaldo_WC2022_-_01.jpg',
+    heroImg: 'img/ronaldo.jpg',
     heroInitials: 'CR',
   },
   {
@@ -91,7 +91,7 @@ const PLAYERS = [
     team: 'Brazil', teamCode: 'Brazil', flag: '🇧🇷', rank: 6, cls: 'A',
     hero: 'Cristiano Ronaldo', heroNation: 'Portugal', heroTop10: true, isCR7: true,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Cristiano_Ronaldo_WC2022_-_01.jpg/240px-Cristiano_Ronaldo_WC2022_-_01.jpg',
+    heroImg: 'img/ronaldo.jpg',
     heroInitials: 'CR',
   },
   {
@@ -99,7 +99,23 @@ const PLAYERS = [
     team: 'England', teamCode: 'England', flag: '🏴󠁧󠁢󠁥󠁮󠁧󠁿', rank: 4, cls: 'A',
     hero: 'Kylian Mbappé', heroNation: 'France', heroTop10: true, isCR7: false,
     heroGoals: 0,
-    heroImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/2019-07-17_SG_Dynamo_Dresden_vs._Paris_Saint-Germain_FC_by_Sandro_Halank%E2%80%93149_%28cropped%29.jpg/240px-2019-07-17_SG_Dynamo_Dresden_vs._Paris_Saint-Germain_FC_by_Sandro_Halank%E2%80%93149_%28cropped%29.jpg',
+    heroImg: 'img/mbappe.jpg',
+    heroInitials: 'KM',
+  },
+  {
+    name: 'SC',
+    team: 'Portugal', teamCode: 'Portugal', flag: '🇵🇹', rank: 5, cls: 'A',
+    hero: 'Lamine Yamal', heroNation: 'Spain', heroTop10: true, isCR7: false,
+    heroGoals: 0,
+    heroImg: 'img/yamal.jpg',
+    heroInitials: 'LY',
+  },
+  {
+    name: 'Ashraf',
+    team: 'Spain', teamCode: 'Spain', flag: '🇪🇸', rank: 2, cls: 'A',
+    hero: 'Kylian Mbappé', heroNation: 'France', heroTop10: true, isCR7: false,
+    heroGoals: 0,
+    heroImg: 'img/mbappe.jpg',
     heroInitials: 'KM',
   },
 ];
